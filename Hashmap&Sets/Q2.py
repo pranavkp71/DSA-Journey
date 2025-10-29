d17e47363ffc6ed29b73b6ce96b864c6b7efc938
pranavkp71 <pranavkp170@gmail.com>
2025-10-29 11:58:34 +0530

    Solve Leetcode 136: Single Number

---
+++ Hashmap&Sets/Q2.py
@@ -0,0 +1,20 @@
+# 136. Single Number
+ 
+# Q) Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+class Solution:
+    def singleNumber(self, nums: int) -> int:
+        result = 0
+        for num in nums:
+            result = num ^ result
+        return result
+            
+
+
+
+obj = Solution()
+print(obj.singleNumber(nums=[4,1,2,1,2]))
+
+
