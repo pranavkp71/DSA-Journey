fe617446da554b9676ecc9ac7320576476c3cd92
pranavkp71 <pranavkp170@gmail.com>
2025-11-03 11:43:13 +0530

    Update Leetcode 977: Used two pointer method

--- Array -String/Q18.py
+++ Array -String/Q18.py
@@ -5,9 +5,18 @@
 
 class Solution:
     def sortedSquares(self, nums):
-        for i in range(len(nums)):
-            nums[i] = nums[i] * nums[i]
-            print(nums)
+        result = []
+
+        i, j = 0, len(nums)-1
+
+        while i <= j:
+            if nums[i] * nums[i] > nums[j] * nums[j]:
+                result.append(nums[i] * nums[i])
+                i += 1
+            else:
+                result.append(nums[j] * nums[j])
+                j -= 1
+        return result[:: -1]
     
 
 obj = Solution()
