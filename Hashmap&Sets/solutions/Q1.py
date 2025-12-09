37b7068d0792c4742e4f81bb7f4f47907cefeba9
pranavkp71 <pranavkp170@gmail.com>
2025-12-09 10:15:45 +0530

    Solve leetcode 242: Valid Anagram

--- Hashmap&Sets/solutions/Q1.py
+++ Hashmap&Sets/solutions/Q1.py
@@ -1,7 +1,3 @@
-# 217. Contains Duplicate
-
-# Q) Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-
 
 class Solution:
     def containsDuplicate(self, nums: int) -> bool:
