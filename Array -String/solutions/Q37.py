9d2c8d3c90e0c80e10f7d189d38ded466e86ebc6
pranavkp71 <pranavkp170@gmail.com>
2026-01-06 11:56:55 +0530

    Solve Leetcode 392: Is Subsequence

---
+++ Array -String/solutions/Q37.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        
+        return i == len(s)
+                    
+
+
+
+obj = Solution()
+print(obj.isSubsequence(s = "abc", t = "ahbgdc"))
